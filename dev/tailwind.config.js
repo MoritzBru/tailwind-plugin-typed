3974613037ef0d660c7df88ff558ba65b964e48c
MoritzBru <moritz_brunnengraeber@hotmail.com>
2024-05-21 22:31:03 +0200

    feat: add darkmode to dev

--- dev/tailwind.config.js
+++ dev/tailwind.config.js
@@ -5,8 +5,9 @@ import typedPlugin from '../dist/index.js';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    resolve(__dirname, 'index.html'),
+    resolve(__dirname, '*.html'),
   ],
+  darkMode: 'media',
   plugins: [
     typographyPlugin,
     typedPlugin,
