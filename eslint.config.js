92f3a10693e454d7b5b6e0c85ab593e09df18a6e
MoritzBru <moritz_brunnengraeber@hotmail.com>
2024-05-22 13:57:50 +0200

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -4,19 +4,23 @@ import tseslint from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
 
 export default [
-  stylistic.configs.customize({
-    indent: 2,
-    quotes: 'single',
-    semi: true,
-    arrowParens: 'always',
-  }),
-  { languageOptions: { globals: { ...globals.node } } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   {
+    languageOptions: {
+      globals: { ...globals.node },
+    },
     ignores: [
       'node_modules/*',
       'dist/*',
     ],
   },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'single',
+    semi: true,
+    arrowParens: true,
+  }),
+
 ];
