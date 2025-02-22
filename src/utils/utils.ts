b31fdfe6c86dff533074a958dbe211ebef77c856
MoritzBru <moritz_brunnengraeber@hotmail.com>
2025-02-22 17:18:51 +0100

    chore: bump deps

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -18,8 +18,8 @@ export function arraySum(arr: number[]) {
 export function flattenColorPalette(colors: RecursiveKeyValuePair = {}): KeyValuePair {
   return Object.assign({}, ...Object.entries(colors).flatMap(([color, values]) => typeof values == 'object'
     ? Object.entries(flattenColorPalette(values)).map(([number, hex]) => ({
-      [color + (number === 'DEFAULT' ? '' : `-${number}`)]: hex,
-    }))
+        [color + (number === 'DEFAULT' ? '' : `-${number}`)]: hex,
+      }))
     : [
         {
           [`${color}`]: values,
